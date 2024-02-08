8a1035962510e58b0962f84d37e93d2d8926d094
unknown <taina.cortez@sou.inteli.edu.br>
2024-02-08 15:39:10 -0300

    ad

---
+++ peixe.js
@@ -0,0 +1,40 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+
+    scene: { 
+        preload: preload,
+        create: create,
+        update: update,
+    }
+};
+
+var game = new Phaser.Game(config);
+
+var peixinho;
+
+function    preload() {
+   this.load.image('peixinho', 'assets/peixes/peixinho_rosa.png');
+   this.load.image('mar', 'assets/bg_azul-escuro.png');
+   this.load.image('logo', 'assets/peixes/logo-inteli_branco.png');
+   this.load.image('peixe', 'assets/peixes/peixe_rosa.png')
+};
+
+
+function    create() {
+    this.add.image(400, 300,'mar');
+    this.add.image(400,525,'logo');
+    this.add.image(700, 300, 'peixe')
+
+    peixinho = this.add.image(400, 300,'peixinho');
+
+
+    peixinho.setFlip(true, false);
+};
+
+
+function update() {
+    peixinho.x = this.input.x;
+    peixinho.y = this.input.y
+}
